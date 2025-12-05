a44b283e0c279c278214773dee3b33384096967e
Owens01 <agunwachidichikere@gmail.com>
2025-12-05 21:11:09 +0100

    updated two files

--- components/GamePage.tsx
+++ components/GamePage.tsx
@@ -42,7 +42,7 @@ export default function GamePage() {
       )}
 
       {/* Game UI - always rendered and visible so canvas can initialize */}
-      <main className="p-2 md:p-8 h-screen w-screen overflow-hidden flex flex-col justify-center">
+      <main className="p-2 md:p-8 w-screen overflow-hidden flex flex-col justify-center">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 lg:gap-4 max-w-7xl mx-auto w-full">
           {/* LEFT/CENTER: Game Canvas */}
           <div className="lg:col-span-2">
